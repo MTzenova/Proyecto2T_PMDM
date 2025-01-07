e8cad69332987aef634fd8815ad664822aca6708
Monica <1692497@alu.murciaeduca.es>
2025-01-07 19:11:18 +0100

    creado activity_main.xml, fragment_register.xml

--- app/src/main/java/com/example/proyecto2t_pmdm/MainActivity.kt
+++ app/src/main/java/com/example/proyecto2t_pmdm/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.proyecto2t_pmdm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.proyecto2t_pmdm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding //Declara una variable de vinculación
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater) //infla al vista utilizando la clase de vinculación
+        val view = binding.root
+
+        //Establece la vista del activity
+        setContentView(view)
+
+        //Ahora puedes acceder a tus vistas directamente usando la variable de vinculación
+
     }
 }
