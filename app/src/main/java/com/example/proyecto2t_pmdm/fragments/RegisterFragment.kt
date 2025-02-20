702b83654ae72f13948cffd828ceccf8a0a0e912
DESKTOP-7QQ3K3B\Moni <monica_97_ct@hotmail.com>
2025-02-20 12:15:48 +0100

    RegisterFragment editado, funcionamiento de los input bien, date tambien

--- app/src/main/java/com/example/proyecto2t_pmdm/fragments/RegisterFragment.kt
+++ app/src/main/java/com/example/proyecto2t_pmdm/fragments/RegisterFragment.kt
@@ -1,11 +1,15 @@
 package com.example.proyecto2t_pmdm.fragments
 
+import android.app.DatePickerDialog
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.proyecto2t_pmdm.R
+import com.example.proyecto2t_pmdm.databinding.FragmentRegisterBinding
+import com.google.android.material.snackbar.Snackbar
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,43 +22,100 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class RegisterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentRegisterBinding?=null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentRegisterBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Asignar el escuchador
+        binding.registrarseRa.setOnClickListener {
+            val clave = binding.campoClaveRa.text.toString()
+            val correo = binding.campoCorreoRa.text.toString()
+            val usuario = binding.campoUsuarioRa.text.toString()
+            var bien = true
+            if (clave.isEmpty() || clave.length < 8) {
+                bien = false
+                binding.claveRa.error = getString(R.string.login_contrasenya_helper)
+            }else{
+                binding.claveRa.error = null
+            }
+            if (correo.isEmpty()) {
+                bien = false
+                binding.correoElectronicoRa.error = getString(R.string.login_correo_helper)
+            }else{
+                binding.correoElectronicoRa.error = null
+            }
+            if (usuario.isEmpty()) {
+                bien = false
+                binding.nombreUsuarioRa.error = getString(R.string.usuario_error_ra)
+            }else{
+                binding.nombreUsuarioRa.error = null
+            }
+            if (bien){
+                val snackRegister = Snackbar.make(binding.root, R.string.snackbar_registrarse_ra, Snackbar.LENGTH_LONG).show()
+                //Intent
+                //val intent: Intent = Intent(this, FavoritosActivity::class.java)
+                //startActivity(intent)
+            }else{
+                val snackError = Snackbar.make(binding.root, R.string.login_error, Snackbar.LENGTH_INDEFINITE).setAction(R.string.snackbar_cerrar)
+                {
+
+                }
+                snackError.show()
+            }
+
+        }
+
+        binding.btnDateRa.setOnClickListener {
+            val cal = java.util.Calendar.getInstance()
+            val dateSetListener = DatePickerDialog.OnDateSetListener {
+                    _, i, i2, i3 ->     cal.set(java.util.Calendar.YEAR, i)
+                cal.set(java.util.Calendar.MONTH, i2)
+                cal.set(java.util.Calendar.DAY_OF_MONTH, i3)
+                binding.dateRa.text = "${cal.get(java.util.Calendar.DAY_OF_MONTH)}" +
+                        "/${cal.get(java.util.Calendar.MONTH)+1}" +
+                        "/${cal.get(java.util.Calendar.YEAR)}"
+
+            }
+
+            DatePickerDialog(requireContext(),
+                dateSetListener, cal.get(java.util.Calendar.YEAR), cal.get(java.util.Calendar.MONTH), cal.get(java.util.Calendar.DAY_OF_MONTH)).show()
+
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+    override fun onDestroyView()
+    {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        //Patrón Singleton
+        private var instance: RegisterFragment? = null
+
+        fun getInstance(): RegisterFragment
+        {
+
+            if (instance == null)
+            {
+                instance = RegisterFragment()
             }
+
+            return instance!!
+        }
+
     }
 }
